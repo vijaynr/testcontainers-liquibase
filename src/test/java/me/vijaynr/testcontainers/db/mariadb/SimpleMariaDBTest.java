0ca2a9ddef129f506b36126e78111a37256ca1f9
Vijay <vj.co.dev@gmail.com>
2024-12-14 14:35:02 +0530

    fix: add simple mariadb test file

---
+++ src/test/java/me/vijaynr/testcontainers/db/mariadb/SimpleMariaDBTest.java
@@ -0,0 +1,119 @@
+package me.vijaynr.testcontainers.db.mariadb;
+
+import me.vijaynr.testcontainers.connections.DBConnectionProvider;
+import me.vijaynr.testcontainers.constants.DatabaseContainerImages;
+import org.junit.jupiter.api.*;
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SimpleMariaDBTest {
+
+    static MariaDBContainer<?> mariadb;
+
+    static {
+        mariadb = new MariaDBContainer<>(DockerImageName.parse(DatabaseContainerImages.MARIADB_IMAGE));
+    }
+
+    static DBConnectionProvider connectionProvider;
+
+    @BeforeAll
+    static void beforeAll() {
+        mariadb.start();
+        connectionProvider = new DBConnectionProvider(
+                mariadb.getJdbcUrl(),
+                mariadb.getUsername(),
+                mariadb.getPassword()
+        );
+    }
+
+    @AfterAll
+    static void afterAll() {
+        mariadb.stop();
+    }
+
+    @BeforeEach
+    void setupSchema() throws SQLException {
+        try (Connection connection = connectionProvider.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate("CREATE TABLE test (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255))");
+        }
+    }
+
+    @AfterEach
+    void tearDown() throws SQLException {
+        try (Connection connection = connectionProvider.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate("DROP TABLE IF EXISTS test");
+        }
+    }
+
+    @Test
+    public void testSimpleQuery() throws SQLException {
+        try (ResultSet resultSet = connectionProvider.getConnection().createStatement().executeQuery("SELECT 1")) {
+            resultSet.next();
+            assertThat(resultSet.getInt(1)).isEqualTo(1);
+        }
+    }
+
+    @Test
+    public void testMariaDBConnection() throws SQLException {
+        try (Connection connection = connectionProvider.getConnection()) {
+            assertThat(connection.isValid(2)).isTrue();
+        }
+    }
+
+    @Test
+    public void testInsertOperation() throws SQLException {
+        try (Connection connection = connectionProvider.getConnection();
+             Statement statement = connection.createStatement()) {
+            int rowsInserted = statement.executeUpdate("INSERT INTO test (name) VALUES ('testName')");
+            assertThat(rowsInserted).isEqualTo(1);
+        }
+    }
+
+    @Test
+    public void testQueryOperation() throws SQLException {
+        try (Connection connection = connectionProvider.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate("INSERT INTO test (name) VALUES ('testName')");
+            try (ResultSet resultSet = statement.executeQuery("SELECT name FROM test WHERE name = 'testName'")) {
+                assertThat(resultSet.next()).isTrue();
+                assertThat(resultSet.getString("name")).isEqualTo("testName");
+            }
+        }
+    }
+
+    @Test
+    public void testUpdateOperation() throws SQLException {
+        try (Connection connection = connectionProvider.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate("INSERT INTO test (name) VALUES ('testName')");
+            int rowsUpdated = statement.executeUpdate("UPDATE test SET name = 'updatedName' WHERE name = 'testName'");
+            assertThat(rowsUpdated).isEqualTo(1);
+            try (ResultSet resultSet = statement.executeQuery("SELECT name FROM test WHERE name = 'updatedName'")) {
+                assertThat(resultSet.next()).isTrue();
+                assertThat(resultSet.getString("name")).isEqualTo("updatedName");
+            }
+        }
+    }
+
+    @Test
+    public void testDeleteOperation() throws SQLException {
+        try (Connection connection = connectionProvider.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate("INSERT INTO test (name) VALUES ('testName')");
+            int rowsDeleted = statement.executeUpdate("DELETE FROM test WHERE name = 'testName'");
+            assertThat(rowsDeleted).isEqualTo(1);
+            try (ResultSet resultSet = statement.executeQuery("SELECT name FROM test WHERE name = 'testName'")) {
+                assertThat(resultSet.next()).isFalse();
+            }
+        }
+    }
+}
